efa33429a917a333e860e5f651972bca058ed5cb
Patrick Dixon <patrick@patrickmdixon.com>
2018-03-25 04:53:06 +0000

    I just can't write code is all

--- vendor/dummy/dummy.go
+++ vendor/dummy/dummy.go
@@ -4,6 +4,6 @@ import "strconv"
 
 func genBody(c *gin.Context, count *int) {
 
-	count = count + 1
+	&count = &count + 1
 	c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"body": strconv.Itoa(count)})
 }
