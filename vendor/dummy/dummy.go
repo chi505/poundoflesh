0140d4333a59d45b5d01d54dae3b81b8d0ea8ea7
Patrick Dixon <patrick@patrickmdixon.com>
2018-03-25 04:48:57 +0000

    This should not matter

--- vendor/dummy/dummy.go
+++ vendor/dummy/dummy.go
@@ -1,9 +1,9 @@
 package dummy
 
 import "strconv"
-	
+
 func genBody(c *gin.Context, count *int) {
 
-		count++
-		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"body": strconv.Itoa(count)})
-		}
+	count++
+	c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"body": strconv.Itoa(count)})
+}
