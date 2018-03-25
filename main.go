b25ae8e3503f81a9a8ff854219ef4f786ace6fa7
Patrick Dixon <patrick@patrickmdixon.com>
2018-03-25 18:19:45 +0000

    Start defining state

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 	"os"
 )
 
+func genResponse(c *gin.Context) {
+	updateState()
+	genBody(c)
+}
+
+func genBody(c *gin.Context) {
+	dummy.GenBody(c)
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -15,6 +24,7 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 	count := &dummy.MutableInt{0}
+	initializeState()
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
@@ -24,7 +34,7 @@ func main() {
 	//		c.String(http.StatusOK, "Hello World")
 	//	})
 	router.GET("/", func(c *gin.Context) {
-		dummy.GenBody(c, count)
+		genResponse(c, count)
 	})
 
 	router.Run(":" + port)
---
+++ state.go
@@ -0,0 +1,19 @@
+package main
+
+type Person struct {
+    Name string
+    State PersonalState
+    
+}
+
+type PersonalState struct {
+    Meat int
+}
+
+func initializeState(){
+    count.Value = 0
+}
+
+func updateState() {
+    
+}
