b9661839f62485de1b635e797598433a3ff23d2b
Patrick Dixon <patrick@patrickmdixon.com>
2018-03-27 19:01:25 +0000

    ...

--- main.go
+++ main.go
@@ -19,14 +19,16 @@ func genBody(c *gin.Context, world WorldState) {
 	GenBody(c, world)
 }
 
+var theWorld WorldState
+
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
-	world := WorldState{MeatLossFrac: 0.01, PerRoundLossFrac: 0.01, NewEntrantMeanAltruism: 10, NewEntrantMeanMeat: MAXMEAT / 10, People: make([]Person, 0, 512)}
-	world.initializeState()
+	theWorld = WorldState{MeatLossFrac: 0.01, PerRoundLossFrac: 0.01, NewEntrantMeanAltruism: 10, NewEntrantMeanMeat: MAXMEAT / 10, People: make([]Person, 0)}
+	theWorld.initializeState()
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
@@ -36,8 +38,8 @@ func main() {
 	//		c.String(http.StatusOK, "Hello World")
 	//	})
 	router.GET("/", func(c *gin.Context) {
-		world.updateState()
-		genResponse(c, world)
+		theWorld.updateState()
+		genResponse(c, theWorld)
 	})
 
 	router.Run(":" + port)
