355d887b161f583a4bbbba664d885973ab6fa767
Patrick Dixon <patrick@patrickmdixon.com>
2018-05-05 19:02:46 +0000

    LOL helps if you save the file

--- state_structs.go
+++ state_structs.go
@@ -1,7 +1,62 @@
-package main
+package states
 
 import "fmt"
-	
-func main() {
-	fmt.Println("Hello from AWS Cloud9!")
+
+var MAXMEAT = 512.0
+var NUMPEOPLE = 20
+var MEATDEC = 5
+
+type Person struct {
+	Name  string
+	State PersonalState
+	ID    int
+}
+
+type MentalState struct {
+    Fear int
+    Hope int
+    Altruism int
+    
+}
+
+type PersonalState struct {
+	Meat     int //quantity
+	MeatBag  []MeatPiece
+	Birthday int
+}
+
+type MeatPiece struct {
+	Name      string //this is outside of Data because used as key into various things
+	Data      MeatData
+	Meat      int
+	OrigOwner string
+}
+
+type MeatData struct {
+	Description string
+}
+
+type WorldState struct {
+	Params     PoundOFleshParams
+	People     []*Person
+	Count      int
+	Assets     TextAssets
+	PersonSpec map[string]MeatSpec
+}
+
+type MeatSpec struct {
+	Count        int
+	MeanInitMeat int
+}
+
+type PoundOFleshParams struct {
+	MeatLossFrac           float64
+	PerRoundLossFrac       float64
+	NewEntrantMeanMeat     int
+	NewEntrantMeanAltruism int
+	UpdateProbPerRound     float64
+}
+
+type TextAssets struct {
+	Organs map[string][]MeatData
 }
