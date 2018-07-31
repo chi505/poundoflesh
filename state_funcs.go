f56044718bfbc75bd01fc8865e4c55394626f0eb
Patrick Dixon <patrick@patrickmdixon.com>
2018-07-31 03:05:16 +0000

    -1 not nil

--- state_funcs.go
+++ state_funcs.go
@@ -88,7 +88,7 @@ func (person *Person) GetMeatIndex(meat *MeatPiece) (int, bool) {
 			return meatIndex, true
 		}
 	}
-	return nil, false
+	return -1, false
 }
 
 func (person *Person) GetMeatByWeight(weight int) (*MeatPiece, bool) {
@@ -100,7 +100,7 @@ func (person *Person) GetMeatByWeight(weight int) (*MeatPiece, bool) {
 			return meatbag[meatIndex], true
 		}
 	}
-	return -1, false
+	return nil, false
 }
 
 //need return value in case we get misaskedfor meat
